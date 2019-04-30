71a1320a4b3d81c70b7827f25b225abcb2ee24a6
Mostafa Yasin <mostafa.a.yasin@gmail.com>
2019-04-30 18:58:15 +0200

    Created 'Expense' class in 'Cars' app.

--- Cars/admin.py
+++ Cars/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import Car
+from .models import Car, Expense
 
 @admin.register(Car)
 class CarAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Expense)
+class ExpenseAdmin(admin.ModelAdmin):
     pass
--- Cars/models.py
+++ Cars/models.py
@@ -10,6 +10,17 @@ class Car(models.Model):
     total_expenses = models.IntegerField()
     panal_no = models.CharField(max_length=20)
     color = models.CharField(max_length=50)
-
+    sold = models.BooleanField(default=False)
+    
     def __str__(self):
         return "%s | %s | %s" % (self.brand, self.model_name, self.model_year)
+
+
+class Expense(models.Model):
+    amount = models.IntegerField()
+    car = models.ForeignKey(Car, on_delete=models.CASCADE)
+    note = models.TextField()
+    date = models.DateTimeField()
+
+    def __str__(self):
+        return "%s | %s | %s" % (self.amount, self.car, self.date)
