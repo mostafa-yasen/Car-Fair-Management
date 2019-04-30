7470655a377eab7fceed29245cd6907d7274b72e
Mostafa Yasin <mostafa.a.yasin@gmail.com>
2019-04-30 16:45:09 +0200

    created the 'Manager' class in 'Employees' app.

--- Employees/models.py
+++ Employees/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Manager(models.Model):
+    name = models.CharField(max_length=120)
+    phone = models.CharField(
+        max_length=12,
+        null=False,
+        blank=False,
+        unique=True
+        )
+    address = models.CharField(max_length=300)
+    age = models.IntegerField(default=20)
+    salary = models.IntegerField()
+    nid = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.name
