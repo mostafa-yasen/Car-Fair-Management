ab220277cc81a1858e53ce02cd41e44730b21349
Mostafa Yasin <mostafa.a.yasin@gmail.com>
2019-04-30 16:59:41 +0200

    created 'Branch' class in 'Branches' app.

--- Branches/models.py
+++ Branches/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from Employees.models import Manager
 
-# Create your models here.
+class Branch(models.Model):
+    name = models.CharField(max_length=120)
+    manager = models.ForeignKey(
+        Manager,
+        on_delete=models.CASCADE
+        )
+    address = models.CharField(max_length=200)
+    capital_equity = models.IntegerField()
+    expenses = models.IntegerField()
+
+    def __str__(self):
+        return self.name
