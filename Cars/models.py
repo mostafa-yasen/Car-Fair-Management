66117aae9f43833021e0d7993499cbe4a15bbd34
Mostafa Yasin <mostafa.a.yasin@gmail.com>
2019-04-30 18:33:02 +0200

    Created 'Car' class in 'Cars' app.

--- Cars/models.py
+++ Cars/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Car(models.Model):
+    brand = models.CharField(max_length=60)
+    model_name = models.CharField(max_length=50)
+    model_year = models.IntegerField()
+    chaisis_no = models.CharField(max_length=200)
+    purchasing_price = models.IntegerField()
+    selling_price = models.IntegerField()
+    total_expenses = models.IntegerField()
+    panal_no = models.CharField(max_length=20)
+    color = models.CharField(max_length=50)
+
+    def __str__(self):
+        return "%s | %s | %s" % (self.brand, self.model_name, self.model_year)
--- carFair/settings.py
+++ carFair/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'Employees',
     'Branches',
     'Clients',
+    'Cars',
 ]
 
 MIDDLEWARE = [
