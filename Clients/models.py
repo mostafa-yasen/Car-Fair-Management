6a5ab6616f359ced9461b54599a90d5c8f114305
Mostafa Yasin <mostafa.a.yasin@gmail.com>
2019-04-30 17:15:36 +0200

    Created 'Client' class in 'Clients' app.

--- Clients/models.py
+++ Clients/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Client(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=12)
+    age = models.IntegerField()
+    driving_licence = models.CharField(max_length=25)
+    notes = models.TextField()
+
+    def __str__(self):
+        return "Mr. %s %s" % (self.first_name, self.last_name)
