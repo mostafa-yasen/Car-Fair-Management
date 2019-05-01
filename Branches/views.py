f87e9836b89a0a03028527fcfba23f6b1acd1c4f
Mostafa Yasin <mostafa.a.yasin@gmail.com>
2019-05-01 07:14:11 +0200

    Initialized 'details' view in 'Branches' app.

--- Branches/views.py
+++ Branches/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Branch
 
+app_name = 'Branches'
 
 def index(request):
     branches = Branch.objects.all()
@@ -9,3 +10,8 @@ def index(request):
     template = 'Branches/branches.html'
     context = {'branches': branches}
     return render(request, template, context=context)
+
+
+def details(request, branch_id):
+    branch = get_object_or_404(Branch, pk=branch_id)
+    return HttpResponse(branch)
